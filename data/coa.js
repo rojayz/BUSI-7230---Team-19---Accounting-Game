01c36e24f24f5777b7741806f74a95d5fb65c93a
rojayz <reed.b.norris@gmail.com>
2026-04-09 15:56:15 -0400

    Update coa.js

--- data/coa.js
+++ data/coa.js
@@ -1,21 +1,20 @@
 export const COA = [
-  { id:"cash", name:"Cash", type:"asset", normal:"D" },
-  { id:"ar", name:"Accounts Receivable", type:"asset", normal:"D" },
-  { id:"supplies", name:"Supplies", type:"asset", normal:"D" },
-  { id:"equip", name:"Equipment", type:"asset", normal:"D" },
+  { id: "cash", name: "Cash", type: "asset", normal: "D" },
+  { id: "ar", name: "Accounts Receivable", type: "asset", normal: "D" },
+  { id: "supplies", name: "Supplies", type: "asset", normal: "D" },
+  { id: "equip", name: "Equipment", type: "asset", normal: "D" },
 
-  { id:"ap", name:"Accounts Payable", type:"liability", normal:"C" },
-  { id:"notes", name:"Notes Payable", type:"liability", normal:"C" },
-  { id:"unearned", name:"Unearned Revenue", type:"liability", normal:"C" },
+  { id: "ap", name: "Accounts Payable", type: "liability", normal: "C" },
+  { id: "notes", name: "Notes Payable", type: "liability", normal: "C" },
+  { id: "unearned", name: "Unearned Revenue", type: "liability", normal: "C" },
 
-  { id:"cs", name:"Common Stock", type:"equity", normal:"C" },
-  { id:"re", name:"Retained Earnings", type:"equity", normal:"C" },
-  { id:"div", name:"Dividends", type:"equity", normal:"D" },
+  { id: "cs", name: "Common Stock", type: "equity", normal: "C" },
+  { id: "div", name: "Dividends", type: "equity", normal: "D" },
 
-  { id:"rev", name:"Service Revenue", type:"revenue", normal:"C" },
+  { id: "rev", name: "Service Revenue", type: "revenue", normal: "C" },
 
-  { id:"rentExp", name:"Rent Expense", type:"expense", normal:"D" },
-  { id:"salExp", name:"Salaries Expense", type:"expense", normal:"D" },
-  { id:"supExp", name:"Supplies Expense", type:"expense", normal:"D" },
-  { id:"depExp", name:"Depreciation Expense", type:"expense", normal:"D" },
+  { id: "rentExp", name: "Rent Expense", type: "expense", normal: "D" },
+  { id: "salExp", name: "Salaries Expense", type: "expense", normal: "D" },
+  { id: "supExp", name: "Supplies Expense", type: "expense", normal: "D" },
+  { id: "depExp", name: "Depreciation Expense", type: "expense", normal: "D" }
 ];
